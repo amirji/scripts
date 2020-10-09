51d30296ca127a0b3dcfa2d32f7a607adccb54d6
amirji <amir.najafgholi@gmail.com>
2020-10-09 10:47:23 +0200

    adding init. env scripts to the rep

---
+++ wak.sh
@@ -0,0 +1,4 @@
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 1 "key ctrl"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 2 "key shift"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 3 "key ctrl z"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 8 "key tab"
---
+++ wax.sh
@@ -0,0 +1,4 @@
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 1 "key shift ctrl p"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 2 "key shift ctrl g"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 3 "key ctrl z"
+xsetwacom set "Wacom Intuos BT S Pad pad" Button 8 "key del"
