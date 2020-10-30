71a3fa4a17e542977c8c3f5144db5576fc914d47
amirji <amir.najafgholi@gmail.com>
2020-10-30 13:14:49 +0100

    make script to laucnh and use notbook via my drawing pad

---
+++ note.sh
@@ -0,0 +1,5 @@
+bluetoothctl connect DC:2C:26:0B:9F:10 >/dev/null 2>&1
+sleep 3
+sh /home/amir/Scripts/wax.sh
+xsetwacom set "Wacom Intuos BT S Pen stylus" MapToOutput DP-1
+xournalpp >/dev/null 2>&1
